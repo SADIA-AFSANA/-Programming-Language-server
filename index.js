88ef89ba3d932d747adb9893ae3903d5e6d29aee
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-10-25 22:56:11 +0600

    server

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
+    res.send('News Api Running and walking')
+});
 
+app.listen(port, () => {
+    console.log('Programming language', port);
 })
